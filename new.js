52de8f5b4751dffd9a378788e0eea6e9f6d4e1f9
kawsar-95 <virtualkawsar@gmail.com>
2020-08-12 03:30:13 +0600

    done

--- new.js
+++ new.js
@@ -8,10 +8,10 @@ class Person {
 const heroPerson = new Person("The", "Godfather", 30000);
 console.log(heroPerson);
 
-// function Person1(firstName, lastName, salary) {
-//   this.firstName = firstName;
-//   this.LastName = lastName;
-//   this.salary = salary;
-// }
-// const oldPerson = new Person1("Anwar", "Uddin", 7000);
+function Person1(firstName, lastName, salary) {
+  this.firstName = firstName;
+  this.LastName = lastName;
+  this.salary = salary;
+}
+const oldPerson = new Person1("Anwar", "Uddin", 7000);
 console.log(oldPerson);
