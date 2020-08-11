3167a9eb9fad6261ad84ddb8b057ac0aa95ff27c
kawsar-95 <virtualkawsar@gmail.com>
2020-08-12 00:55:52 +0600

    done

---
+++ new.js
@@ -0,0 +1,17 @@
+class Person {
+  constructor(firstName, lastName, salary) {
+    this.firstName = firstName;
+    this.LastName = lastName;
+    this.salary = salary;
+  }
+}
+const heroPerson = new Person("The", "Godfather", 30000);
+console.log(heroPerson);
+
+// function Person1(firstName, lastName, salary) {
+//   this.firstName = firstName;
+//   this.LastName = lastName;
+//   this.salary = salary;
+// }
+// const oldPerson = new Person1("Anwar", "Uddin", 7000);
+console.log(oldPerson);
