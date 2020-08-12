f2b044b41047f136db8ce2b3e1bcb2e8a39af3e9
kawsar-95 <virtualkawsar@gmail.com>
2020-08-12 17:55:54 +0600

    done

--- timeOut.js
+++ timeOut.js
@@ -2,6 +2,15 @@ function doSomething() {
   console.log("34567");
 }
 console.log("123344");
-setTimeout(doSomething);
+// setTimeout(doSomething, 4000);
+// setTimeout(function name(params) {
+//   console.log("waiting");
+// }, 10000);
+// setTimeout(() => {
+//   console.log("See You Later");
+// });
+setInterval(function () {
+  console.log("do it");
+}, 3000);
 console.log("454673890");
 console.log("12345678");
